bb26f6f4b319a548d00ef1de0002ad571375d8fa
yoonki1207 <yoonki1207@naver.com>
2023-03-12 01:15:26 +0900

    [Feat] Database settings

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var authRouter = require("./routes/auth");
 
+// database settings
+require("dotenv").config({
+  path: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.production",
+});
+var database = require("./database/database");
+
 var app = express();
 
 // view engine setup
@@ -28,18 +34,18 @@ app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get("env") === "development" ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render("error");
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
