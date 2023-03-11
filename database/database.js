bb433027927276fc54eaa75ee34c0615b85ad6a2
yoonki1207 <yoonki1207@naver.com>
2023-03-12 01:16:38 +0900

    [Feat] Move env settings to app.js

--- database/database.js
+++ database/database.js
@@ -1,15 +1,12 @@
 const mysql = require("mysql"); // load mysql library
 
 // NODE_ENV 별 사용할 파일 세팅
-require("dotenv").config({
-	path: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.production",
-});
 
 const database = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 database.connect();
