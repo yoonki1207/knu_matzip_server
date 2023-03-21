6e5f0e408aa2995233595b20d160e197a7209a12
yoonki1207 <yoonki1207@github.com>
2023-03-21 13:51:09 +0900

    [Comment] 주석 수정

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
-var winston = require("winston")
+var winston = require("winston");
 var morgan = require("morgan");
-var {createProxyMiddleware} = require("http-proxy-middleware");
+var { createProxyMiddleware } = require("http-proxy-middleware");
 
-
-// database settings
+// node env settings
 require("dotenv").config({
 	path: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.production",
 });
 
+// database settings
 var database = require("./database/database");
 
 // import routers
@@ -22,26 +22,32 @@ var mapsRouter = require("./routes/kakaomap");
 var recmdRouter = require("./routes/recmd");
 
 var app = express();
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 // view engine setup
 // app.set("views", path.join(__dirname, "views"));
 // app.set("view engine", "jade");
 
 app.use((req, res, next) => {
-	req.headers['x-forwarded-for'] = req.connection.remoteAddress;
+	req.headers["x-forwarded-for"] = req.connection.remoteAddress;
 	next();
 });
 
-app.use('/api', createProxyMiddleware({target: 'http://localhost:3002', changeOrigin:true}));
+app.use(
+	"/api",
+	createProxyMiddleware({ target: "http://localhost:3002", changeOrigin: true })
+);
 
-app.use(morgan({
-	  format: ':req[X-Forwarded-For] (:remote-addr) :status :method :url :referrer :response-time ms :user-agent',
-	  stream: {
-		write: (message) => {
-			winston.info(message);
-		}
-	  },
-}));
+app.use(
+	morgan({
+		format:
+			":req[X-Forwarded-For] (:remote-addr) :status :method :url :referrer :response-time ms :user-agent",
+		stream: {
+			write: (message) => {
+				winston.info(message);
+			},
+		},
+	})
+);
 //app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -57,7 +63,7 @@ app.use("/recmd", recmdRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	console.log(req.headers['x-forwarded-for'] || req.ip);
+	console.log(req.headers["x-forwarded-for"] || req.ip);
 	next(createError(404));
 });
 
