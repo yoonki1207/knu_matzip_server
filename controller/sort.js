0f386966d095c4081e5c45ac8248fb78104ffce2
yoonki1207 <yoonki1207@naver.com>
2023-04-02 19:10:47 +0900

    [Comment] sort 구현해야할 것

---
+++ controller/sort.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+// "정렬" Controller. 정렬된 식당을 응답한다.
+
+// 랜덤
+
+// 인기 순. 자세한 알고리즘은 Notion에.
+
+// 평점 많은 순
+
+// 별점 높은 순
+
+// 별점 낮은 순
+
+// 좋아요 많은 순
+
+// 좋아요 적은 순
