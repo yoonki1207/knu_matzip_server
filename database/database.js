5fdfb3056805b574433a2e4366a5059e0a25a07f
yoonki1207 <yoonki1207@gmail.com>
2023-04-30 18:30:50 +0900

    [Refactor] sql refactor

--- database/database.js
+++ database/database.js
@@ -13,98 +13,158 @@ const database = mysql.createPool({
 const init = async () => {
 	console.log("Create Database Tables...");
 	try {
-		const resultUsrtbl = await database.query(
-			`CREATE TABLE IF NOT EXISTS refresh_token(
-        id int AUTO_INCREMENT not null PRIMARY KEY,
-        access_token char(200) not null,
-        refresh_token char(200) not null,
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP, -- 가입일
-        updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        deleted_at DATETIME
-      );`
-		);
-		// 유저
-		await database.query(
-			`CREATE TABLE IF NOT EXISTS usertbl(
-        user_id int AUTO_INCREMENT not null PRIMARY KEY comment '회원 고유번호', -- 회원 고유번호
-        name varchar(24) not null comment '회원 실명', -- 실명
-        birth_year Date not null comment '회원 태어난 년도',
-        phone_number varchar(20) not null UNIQUE KEY comment '회원 전화번호', -- (UK)
-        email varchar(30) not null UNIQUE KEY comment 'email 겸 로그인 아이디', -- email 겸 로그인 아이디(UK)
-        password varchar(200) not null comment '비밀번호',
-        nickname char(16) null UNIQUE KEY comment '별명', -- (UK)
-        gender char(4) comment '성별',
-        role varchar(20) comment '직업', -- 직업 // GUEST(Not KNU Member), VAILD, ADMIN
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP, -- 가입일
-        updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        deleted_at DATETIME,
-        token_id int,
-        FOREIGN KEY (token_id) REFERENCES refresh_token(id)
-      );`
-		);
+		const resultUsrtbl = await database.query(`
+			-- MySQL Workbench Forward Engineering
 
-		// 가게정보
-		await database.query(
-			`CREATE TABLE IF NOT EXISTS storetbl(
-        id char(16) not null PRIMARY KEY, -- 가게 고유번호
-        category_group_code varchar(10) not null,
-        category_group_name varchar(100) not null,
-        phone varchar(15) not null,
-        place_name varchar(100) not null,
-        place_url varchar(100) not null,
-        road_address_name varchar(100) not null,
-        x varchar(100) not null,
-        y varchar(100) not null,
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP, -- 가입일
-        updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        deleted_at DATETIME
-      );`
-		);
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-		// 리뷰
-		await database.query(
-			`CREATE TABLE IF NOT EXISTS reviewtbl(
-        id int AUTO_INCREMENT not null PRIMARY KEY,
-        store_id char(16) not null, -- FK (storetbl)
-        user_id int, -- FK (usertbl)
-        nickname char(16), -- FK(usertbl)
-        content varchar(100), -- 리뷰 내용
-        rating int(16), -- 평점 
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP, -- 가입일
-        updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        deleted_at DATETIME,
-    
-        FOREIGN KEY (store_id) REFERENCES storetbl(id),
-        FOREIGN KEY (user_id) REFERENCES usertbl(user_id),
-        FOREIGN KEY (nickname) REFERENCES usertbl(nickname)
-    );`
-		);
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
 
-		// 가게 태그
-		await database.query(
-			`CREATE TABLE IF NOT EXISTS tag_storetbl(
-        id int AUTO_INCREMENT not null PRIMARY KEY,
-        store_id char(16) not null,
-        category_name varchar(20) not null,
-        
-        FOREIGN KEY (store_id) REFERENCES storetbl(id)
-        );`
-		);
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS 'mydb' DEFAULT CHARACTER SET utf8 ;
+USE 'mydb' ;
 
-		// 찜 목록
-		await database.query(
-			`CREATE TABLE IF NOT EXISTS like_list(
-        id int AUTO_INCREMENT not null PRIMARY KEY,
-        store_id char(16) not null,
-        user_id int not null,
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP, -- 가입일
-        updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        deleted_at DATETIME,
-      
-        FOREIGN KEY (store_id) REFERENCES storetbl(id),
-        FOREIGN KEY (user_id) REFERENCES usertbl(user_id)
-    );`
-		);
+-- -----------------------------------------------------
+-- Table 'mydb'.'usertbl'
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS 'mydb'.'usertbl' (
+  'user_id' INT NOT NULL AUTO_INCREMENT,
+  'email' VARCHAR(45) NOT NULL,
+  'password' VARCHAR(45) NOT NULL,
+  'nickname' VARCHAR(45) NOT NULL,
+  'phone_number' VARCHAR(45) NULL,
+  'birth_year' VARCHAR(45) NOT NULL,
+  'gender' VARCHAR(45) NOT NULL,
+  'role' VARCHAR(45) NOT NULL,
+  'created_at' TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  'updated_at' TIMESTAMP NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  'deleted_at' TIMESTAMP NULL DEFAULT NULL,
+  'login_date' VARCHAR(45) NULL,
+  PRIMARY KEY ('user_id'),
+  UNIQUE INDEX 'email_UNIQUE' ('email' ASC) VISIBLE,
+  UNIQUE INDEX 'nickname_UNIQUE' ('nickname' ASC) VISIBLE,
+  UNIQUE INDEX 'phone_number_UNIQUE' ('phone_number' ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table 'mydb'.'storetbl'
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS 'mydb'.'storetbl' (
+  'id' VARCHAR(45) NOT NULL,
+  'category_group_code' VARCHAR(45) NULL,
+  'category_group_name' VARCHAR(45) NULL,
+  'phone' VARCHAR(45) NULL,
+  'place_name' VARCHAR(45) NULL,
+  'place_url' VARCHAR(45) NULL,
+  'road_address_name' VARCHAR(45) NULL,
+  'x' VARCHAR(45) NULL,
+  'y' VARCHAR(45) NULL,
+  'created_at' TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  'updated_at' TIMESTAMP NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  'deleted_at' TIMESTAMP NULL DEFAULT NULL,
+  'storetblcol' VARCHAR(45) NULL,
+  PRIMARY KEY ('id'))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table 'mydb'.'reviewtbl'
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS 'mydb'.'reviewtbl' (
+  'store_id' VARCHAR(45) NOT NULL,
+  'user_id' INT NOT NULL,
+  'content' VARCHAR(45) NULL,
+  'rating' INT NOT NULL,
+  'created_at' TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  'updated_at' TIMESTAMP NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  'deleted_at' TIMESTAMP NULL DEFAULT NULL,
+  INDEX 'store_id_idx' ('store_id' ASC) VISIBLE,
+  INDEX 'user_id_idx' ('user_id' ASC) VISIBLE,
+  PRIMARY KEY ('user_id', 'store_id'),
+  CONSTRAINT 'store_id'
+    FOREIGN KEY ('store_id')
+    REFERENCES 'mydb'.'storetbl' ('id')
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT 'user_id'
+    FOREIGN KEY ('user_id')
+    REFERENCES 'mydb'.'usertbl' ('user_id')
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table 'mydb'.'like_list'
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS 'mydb'.'like_list' (
+  'store_id' VARCHAR(45) NOT NULL,
+  'user_id' INT NOT NULL,
+  'created_at' TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  'updated_at' TIMESTAMP NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  'deleted_at' TIMESTAMP NULL DEFAULT NULL,
+  'like_listcol' VARCHAR(45) NULL,
+  INDEX 'store_id_idx' ('store_id' ASC) VISIBLE,
+  INDEX 'user_id_idx' ('user_id' ASC) VISIBLE,
+  PRIMARY KEY ('store_id', 'user_id'),
+  CONSTRAINT 'store_id0'
+    FOREIGN KEY ('store_id')
+    REFERENCES 'mydb'.'storetbl' ('id')
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT 'user_id0'
+    FOREIGN KEY ('user_id')
+    REFERENCES 'mydb'.'usertbl' ('user_id')
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table 'mydb'.'tag_storetbl'
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS 'mydb'.'tag_storetbl' (
+  'id' INT NOT NULL,
+  'store_id' VARCHAR(45) NOT NULL,
+  'category_name' VARCHAR(45) NULL,
+  PRIMARY KEY ('id'),
+  INDEX 'store_id_idx' ('store_id' ASC) VISIBLE,
+  UNIQUE INDEX 'store_id_UNIQUE' ('store_id' ASC) VISIBLE,
+  CONSTRAINT 'store_id1'
+    FOREIGN KEY ('store_id')
+    REFERENCES 'mydb'.'storetbl' ('id')
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table 'mydb'.'refresh_token'
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS 'mydb'.'refresh_token' (
+  'id' INT NOT NULL,
+  'access_token' CHAR(200) NOT NULL,
+  'refresh_token' CHAR(200) NOT NULL,
+  'created_at' TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  'updated_at' TIMESTAMP NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  'deleted_at' TIMESTAMP NULL DEFAULT NULL,
+  'refresh_tokencol' VARCHAR(45) NULL,
+  PRIMARY KEY ('id'))
+ENGINE = InnoDB
+KEY_BLOCK_SIZE = 1;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+`);
 
 		return database;
 	} catch (error) {
