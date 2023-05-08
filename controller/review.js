510c8786b397f62e138bbe141029a43cee910983
SeHyun OH <valy0008@gamil.com>
2023-05-08 13:41:57 +0900

    [update] review CRUD logic change

--- controller/review.js
+++ controller/review.js
@@ -9,35 +9,52 @@ router.post("/:store_id", userAuth, async (req, res, next) => {
 	const review = await reviewService.writeReview(req.body.store_id, req.body.user_id, req.body.content, req.body.rating);
 	if (!review) { 
 		console.log('failed')
-		res.send("failed create review")
+		res.status(400).send("failed create review")
 		return; 
 	}
 	// 로그인된 회원만 리뷰 입력 가능
-	if(review){
-		if(review[1] == userAuth){
-			res.render('');
-		}
-		else{
-			res.send("로그인된 회원만 리뷰 쓰기 가능")
-		}
+	const user = user.find(u => u.user_id === parseInt(req.params.user_id))
+	if(!user){
+		res.status(404).send('Please login - only member can review')
+		return;
 	}
+	res.send(review);
 });
 
 /* Get reviews from a borad */
 /* Read review */
-router.get("/:store_id", async (req, res, next) => {
+router.get("/", async (req, res, next) => {
 	const review = await reviewService.getReview(req.body.store_id);
 	if (!review) { 
 		console.log('failed')
+		res.status(400);
 		res.send("failed read review")
 		return; 
 	}
-	
+	res.render('');
+	res.send(review);
 });
 
 /* Update reviews by id */
 /* Update review */
-router.put("/:store_id", async (req, res, next) => {});
+router.put("/:store_id", async (req, res, next) => {
+	const review = await reviewService.rewriteReview(req.content, req.body.rating, req.body.store_id, req.body.user_id);
+	if (!review) { 
+		console.log('failed')
+		res.status(400);
+		res.send("failed update review")
+		return; 
+	}
+	res.send(review);
+});
 
 /* delete reviews */
-router.delete("/:store_id", async (req, res, next) => {});
+router.delete("/:store_id", async (req, res, next) => {
+	if (!review) { 
+		console.log('failed')
+		res.status(400);
+		res.send("failed delete review")
+		return; 
+	}
+	res.send(review);
+});
