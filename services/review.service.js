61d93be021417630cb9a6aa483ada1fd8575b450
yoonki1207 <yoonki1207@gmail.com>
2023-05-15 15:50:01 +0900

    [Commit] 함수 docs 추가

--- services/review.service.js
+++ services/review.service.js
@@ -1,6 +1,11 @@
 // reviewRepository.js 에서 얻어온 정보를 바탕으로 가공후 controller인 review.js에게 보냄.
 
-const { createReview, readReview, updateReview, deleteReview } = require("../repository/reviewRepository");
+const {
+	createReview,
+	readReview,
+	updateReview,
+	deleteReview,
+} = require("../repository/reviewRepository");
 
 /**
  *
@@ -21,13 +26,22 @@ const getReview = async (store_id) => {
 };
 
 // Review 수정
-const rewriteReview = async(content, rating, store_id, user_id) => {
+const rewriteReview = async (content, rating, store_id, user_id) => {
+	if (rating > 10 || rating < 1) {
+		return "Rating range exception.";
+	}
 	return await updateReview(content, rating, store_id, user_id);
 };
 
 // Review 삭제
-const delReview = async(store_id, user_id) => {
+const delReview = async (store_id, user_id) => {
 	return await deleteReview(store_id, user_id);
 };
 
-module.exports = { writeReview, getReview, rewriteReview, delReview, readReview};
+module.exports = {
+	writeReview,
+	getReview,
+	rewriteReview,
+	delReview,
+	readReview,
+};
