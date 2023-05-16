9725a3ab8eb8f6457daa01c44df55339fbb8e242
yoonki1207 <yoonki1207@naver.com>
2023-05-16 20:45:16 +0900

    [Hotfix] 인증 예외 처리

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -6,54 +6,52 @@ const authService = require("../services/auth.service");
  * @param {NextFunction} next
  */
 const userAuth = async (req, res, next) => {
-	if (
-		req.headers.authorization === undefined ||
-		req.headers.authorization === null
-	) {
-		res.status(400).send("Required Bearer token");
-	}
-	const access_token = req.headers.authorization.split("Bearer ")[1];
-	const verify = await authService.verifyToken(access_token);
-	if (!verify) {
-		// AT is invalid. It can be expried or not a token.
-		const isExpired = await authService.isExpriedToken(access_token);
-		console.log(isExpired);
-		if (!isExpired || isExpired === null) {
-			res.status(400).send("Valid access token requried.");
-			return;
-		} else {
-			// AT is valid and expried.
-			const refresh_token = req.headers["refresh_token"];
-			if (!refresh_token) {
-				res.status(400).send("Try request with refresh_token again.");
-			}
-			const result = await authService.findTokenWithRT(refresh_token);
-			console.log("AC", result.access_token);
-			console.log("AS", access_token);
-			if (result.access_token === access_token) {
-				// re-authorization.
-				const payload = authService.getPayloadByToken(refresh_token);
-				const nat = authService.createAccessToken(
-					payload.email,
-					payload.nickname
-				);
-				res.cookie("access_token", nat);
-				req.user = await authService.getUser(payload.email);
-				console.log("RT", payload);
-				next();
+	try {
+		const access_token = req.headers.authorization.split("Bearer ")[1];
+		const verify = await authService.verifyToken(access_token);
+		if (!verify) {
+			// AT is invalid. It can be expried or not a token.
+			const isExpired = await authService.isExpriedToken(access_token);
+			console.log(isExpired);
+			if (!isExpired || isExpired === null) {
+				res.status(400).send("Valid access token requried.");
+				return;
 			} else {
-				// TODO: return false and drop the record. Client should to login again.
-				console.log("Required RT.", refresh_token);
-				res.status(400).send("Requied RT.");
+				// AT is valid and expried.
+				const refresh_token = req.headers["refresh_token"];
+				if (!refresh_token) {
+					res.status(400).send("Try request with refresh_token again.");
+				}
+				const result = await authService.findTokenWithRT(refresh_token);
+				console.log("AC", result.access_token);
+				console.log("AS", access_token);
+				if (result.access_token === access_token) {
+					// re-authorization.
+					const payload = authService.getPayloadByToken(refresh_token);
+					const nat = authService.createAccessToken(
+						payload.email,
+						payload.nickname
+					);
+					res.cookie("access_token", nat);
+					req.user = await authService.getUser(payload.email);
+					console.log("RT", payload);
+					next();
+				} else {
+					// TODO: return false and drop the record. Client should to login again.
+					console.log("Required RT.", refresh_token);
+					res.status(400).send("Requied RT.");
+				}
+				return;
 			}
-			return;
+		} else {
+			// AT is valid.
+			const payload = authService.getPayloadByToken(access_token);
+			req.user = await authService.getUser(payload.email);
+			console.log("AT", payload);
+			next();
 		}
-	} else {
-		// AT is valid.
-		const payload = authService.getPayloadByToken(access_token);
-		req.user = await authService.getUser(payload.email);
-		console.log("AT", payload);
-		next();
+	} catch (e) {
+		res.status(400).send("Required Beaer token");
 	}
 	return;
 };
