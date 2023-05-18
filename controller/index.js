4d4a9052d6944774f2c901b6c4f0bf8ababd2f53
yoonki1207 <yoonki1207@gmail.com>
2023-05-18 17:35:13 +0900

    [Refactor] index.js 응답 객체 적용

--- controller/index.js
+++ controller/index.js
@@ -1,9 +1,10 @@
 var express = require("express");
+const responseBody = require("../utils/responseBody");
 var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-	res.send("Knu matzip RESTApi from Express. 0.0.1");
+	res.send(responseBody(200, "Knu matzip RESTApi from Express. 0.0.1", true));
 });
 
 module.exports = router;
