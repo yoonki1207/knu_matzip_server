dcd04867485cf8453ef095fcb62d35e37db17fd5
yoonki1207 <yoonki1207@gmail.com>
2023-05-25 14:50:36 +0900

    [Refact] 코드 리팩토링

--- controller/recmd.js
+++ controller/recmd.js
@@ -1,7 +1,6 @@
 // 추천 모델 컨트롤러.
 const express = require("express");
 const { userAuth } = require("../middlewares/authentication");
-const responseBody = require("../utils/responseBody");
 const recmdService = require("../services/recmd.servcie");
 const router = express.Router();
 
@@ -9,9 +8,6 @@ const router = express.Router();
 router.get("/popular", recmdService.getPopular);
 
 // 사용자별 맞춤 아이템 api
-router.get("/user-recommend", async (req, res) => {
-	const access_token = req.headers.authorization.split("Bearer ")[1];
-	const user = getUserByToken(access_token);
-});
+router.get("/user-recommend", recmdService.userRecommend);
 
 module.exports = router;
--- services/recmd.servcie.js
+++ services/recmd.servcie.js
@@ -1,17 +1,24 @@
-const connection = require("../database/database");
 const axios = require("axios");
-const jwt = require("jsonwebtoken");
-const database = require("../database/database");
+const authService = require("../services/auth.service");
 const { getStoreIds } = require("../repository/recmdRepository");
 const responseBody = require("../utils/responseBody");
 
 const getPopular = async (req, res) => {
+	const data = await getInfos();
+	res.send(responseBody("응답 성공.", data));
+};
+
+/**
+ * It returns dummy infos
+ * @returns {Promise<{id: string, title: string, image_url: string}>}
+ */
+const getInfos = async () => {
 	const urls = await getStoreIds();
 	const result = await _getImageUrls([...urls.map((val) => val.id)]);
 	const data = urls.map((val, index) => {
 		return { ...val, image_url: result[index] };
 	});
-	res.send(responseBody("응답 성공.", data));
+	return data;
 };
 
 /**
@@ -32,9 +39,11 @@ const _getImageUrls = async (images) => {
 				return `Error: Cannot parse to Integer with ${place_url}`;
 			}
 			const result = await axios.get(URL + place_number, {
-				headers: {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',},
-			}
-			);
+				headers: {
+					"User-Agent":
+						"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+				},
+			});
 			const basicInfo = result.data.basicInfo;
 			return await basicInfo.mainphotourl;
 		};
@@ -44,4 +53,27 @@ const _getImageUrls = async (images) => {
 	return urls;
 };
 
-module.exports = { getPopular };
+/**
+ * 유저별 맞춤형 추천 데이터 제공.
+ * @param {Request} req
+ * @param {Response} res
+ */
+const userRecommend = async (req, res) => {
+	const access_token = req.headers.authorization.split("Bearer ")[1];
+	const paylaod = authService.getPayloadByToken(access_token);
+	const user = await authService.getUser(paylaod.email);
+	console.log(user);
+	if (user.email !== null || user.email !== undefined) {
+		// 유저 맞춤형 추천
+		const data = await getInfos();
+		console.log(data);
+		res.send(responseBody(`${user.nickname} 맞춤형 추천.`, data));
+	} else {
+		// 비회원 유저 맞춤형 추천
+		const data = await getInfos();
+		res.send(responseBody(`비회원 추천`, data));
+	}
+	return;
+};
+
+module.exports = { getPopular, userRecommend };
